87b87134a3fe6f1e77f8ee05c06bfa46c5e5e732
s1rius <cnsirius81@163.com>
2023-08-11 15:31:04 +0800

    Site updated: 2023-08-11 15:31:01

--- custom/nav.js
+++ custom/nav.js
@@ -1,26 +1,37 @@
-document.addEventListener('pjax:complete', tonav);
-document.addEventListener('DOMContentLoaded', tonav);
+document.addEventListener("pjax:complete", tonav);
+document.addEventListener("DOMContentLoaded", tonav);
 //响应pjax
 function tonav() {
-    document.getElementById("name-container").setAttribute("style", "display:none");
-    var position = $(window).scrollTop();
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll > position) {
-            document.getElementById("name-container").setAttribute("style", "");
-            document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
-        } else {
-            document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
-            document.getElementById("name-container").setAttribute("style", "display:none");
-        }
-        position = scroll;
-    });
-    //修复没有弄右键菜单的童鞋无法回顶部的问题
-    document.getElementById("page-name").innerText = document.title.split(" | Fomalhaut🥝")[0];
+  document
+    .getElementById("name-container")
+    .setAttribute("style", "display:none");
+  var position = $(window).scrollTop();
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > position) {
+      document.getElementById("name-container").setAttribute("style", "");
+      document
+        .getElementsByClassName("menus_items")[1]
+        .setAttribute("style", "display:none!important");
+    } else {
+      document
+        .getElementsByClassName("menus_items")[1]
+        .setAttribute("style", "");
+      document
+        .getElementById("name-container")
+        .setAttribute("style", "display:none");
+    }
+    position = scroll;
+  });
+  //修复没有弄右键菜单的童鞋无法回顶部的问题
+  document.getElementById("page-name").innerText =
+    document.title.split(" | Fomalhaut🥝")[0];
 }
 
 function scrollToTop() {
-    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
-    document.getElementById("name-container").setAttribute("style", "display:none");
-    btf.scrollToDest(0, 500);
+  document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+  document
+    .getElementById("name-container")
+    .setAttribute("style", "display:none");
+  btf.scrollToDest(0, 500);
 }
